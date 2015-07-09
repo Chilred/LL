12b0611aaa384193e1de69704c6de13bc38882e3
m4lte <malte.schoon@iem.thm.de>
2015-07-09 14:37:14 +0200

    Designanpassungen, Umstrukturierungen, Buttontexte hinzugefügt, "Training" Menüpunkt immer markiert, wenn Trainingsartikel aktiv

--- _demo/src/js/mark-menu-entry.js
+++ _demo/src/js/mark-menu-entry.js
@@ -18,11 +18,16 @@ App.NavigationManager.activeLink = (function() {
 
 	return function() {
 		var menuItemToBeActivated = $("article[data-menu-id='" + App.SectionManager.articles[arguments[0]].selector.data('mark-menu') + "']").attr('id');
+		var isTraining = App.SectionManager.articles[arguments[0]].selector.closest('section').data('is-training');
+		
 		if(menuItemToBeActivated) {
 			$(".mainNavigation a[data-target='" + menuItemToBeActivated + "']").closest('li').addClass('active');
-			$(".subNavigation a[data-target='" + menuItemToBeActivated + "']").addClass('active');
+			$(".subNavigation a[data-target='" + menuItemToBeActivated + "']").addClass('active');			
 		} else {
 			cached_function.apply(this, arguments);
+		}
+		if(isTraining) {
+			$(".extra-nav").find('li').first().addClass('active');
 		}
 	};
 }());
@@ -32,11 +37,16 @@ App.NavigationManager.inactiveLink = (function() {
 
 	return function() {
 		var menuItemToBeDeactivated = $("article[data-menu-id='" + App.SectionManager.articles[arguments[0]].selector.data('mark-menu') + "']").attr('id');
+		var isTraining = App.SectionManager.articles[arguments[0]].selector.closest('section').data('is-training');
+		
 		if(menuItemToBeDeactivated) {
 			$(".mainNavigation a[data-target='" + menuItemToBeDeactivated + "']").closest('li').removeClass('active');
 			$(".subNavigation a[data-target='" + menuItemToBeDeactivated + "']").removeClass('active');
 		} else {
 			cached_function.apply(this, arguments);
 		}
+		
+		//remove "training" mark
+		$(".extra-nav").find('li').first().removeClass('active');
 	};
 }());
